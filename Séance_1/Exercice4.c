55260633d61aa9f596042cc769a5be80ac90b6f6
Tom <tommusique974@gmail.com>
2022-01-29 13:44:13 +0100

    feat(*) : Comprendre la notion de types et les conversions automatiques

---
+++ Séance_1/Exercice4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    unsigned int nombre;
+
+    printf("Saisir un nombre : ");
+    scanf("%d", &nombre);
+
+    printf("Votre nombre : %d", nombre);
+    return 0;
+}
+
+// bha c'est la même, ça marche
